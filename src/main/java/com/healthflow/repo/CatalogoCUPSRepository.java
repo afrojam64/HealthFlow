5913922679aa0a27be7eadb09071a7533d9d1ae6
Afrojam <afrojam64@gmail.com>
2026-04-27 17:27:30 -0500

    Ajuste en formulario CUPS - V2

--- src/main/java/com/healthflow/repo/CatalogoCUPSRepository.java
+++ src/main/java/com/healthflow/repo/CatalogoCUPSRepository.java
@@ -1,11 +1,11 @@
 package com.healthflow.repo;
 
 import com.healthflow.domain.CatalogoCUPS;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
