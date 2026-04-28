a16e6eb4b58bd0b558c39c911b8802b7ad460545
Afrojam <afrojam64@gmail.com>
2026-04-28 13:59:49 -0500

    Reparación generación Json - RIPS

--- src/main/java/com/healthflow/repo/RipsGenerationRepository.java
+++ src/main/java/com/healthflow/repo/RipsGenerationRepository.java
@@ -13,8 +13,13 @@ public interface RipsGenerationRepository extends JpaRepository<RipsGeneration, 
 
     List<RipsGeneration> findByProfessionalIdOrderByFechaGeneracionDesc(UUID professionalId);
 
-    @Query("SELECT MAX(CAST(SUBSTRING(r.numFactura, 5) AS int)) FROM RipsGeneration r " +
+    /*@Query("SELECT MAX(CAST(SUBSTRING(r.numFactura, 5) AS int)) FROM RipsGeneration r " +
             "WHERE r.professionalId = :professionalId AND r.numFactura LIKE 'REC-%'")
+    Integer findMaxConsecutivoReciboInterno(@Param("professionalId") UUID professionalId);*/
+
+    @Query(value = "SELECT MAX(CAST(SPLIT_PART(r.num_factura, '-', 3) AS int)) " +
+            "FROM rips_generations r " +
+            "WHERE r.professional_id = :professionalId AND r.num_factura LIKE 'REC-%'", nativeQuery = true)
     Integer findMaxConsecutivoReciboInterno(@Param("professionalId") UUID professionalId);
 
     // En RipsGenerationRepository.java, agregar:
